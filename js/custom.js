fa78384140faa542e45a793e74abd68c8e936614
abdMasud <abdmasud.2000@gmail.com>
2024-08-17 20:42:37 +0600

    deploy 1

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+$('.soft-sliders').slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    // dots: true,
+    // infinity: true,
+    // speed: 300,
+});
