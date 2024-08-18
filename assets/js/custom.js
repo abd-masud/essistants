bf7cc5b71847588b438bbcaea37ca4f0d1adb67d
abdMasud <abdmasud.2000@gmail.com>
2024-08-18 11:18:26 +0600

    responsive and modal

---
+++ assets/js/custom.js
@@ -0,0 +1,11 @@
+$('.soft-sliders').slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    dots: true,
+    infinity: true,
+    speed: 500,
+});
+
+$('.testimonials-sliders').slick();
