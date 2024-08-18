4f3fb4efe5bdc949ba6064b721a9e22eb279a881
abdMasud <abdmasud.2000@gmail.com>
2024-08-18 15:39:58 +0600

    software slider

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,14 +1,45 @@
 $('.soft-sliders').slick({
+    dots: false,
+    autoplay: true,
+    infinite: true,
+    speed: 500,
     slidesToShow: 6,
     slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    dots: true,
-    infinity: true,
-    speed: 500,
+    autoplaySpeed: 2000,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 5,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+            }
+        },
+    ]
 });
 
-$('.testimonials-sliders').slick();
+$('.testimonials-sliders').slick({
+    dots: false,
+    autoplay: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplaySpeed: 2000,
+});
 
 document.getElementById("projectForm").addEventListener("submit", function (event) {
     event.preventDefault();
